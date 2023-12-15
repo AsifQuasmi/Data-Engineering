dc5c773c87878645658f7b13c2f8d5189e0c8dcb
Asif Quasmi <asifquasmi11@gmail.com>
2023-12-16 00:51:00 +0530

    Adding Hello file

---
+++ Hello.py
@@ -0,0 +1,41 @@
+print("Hello Asif")
+
+
+
+print("Asif's Quasmi")
+
+a='10  '
+print(5 * a)
+
+print(10//3)
+
+
+
+nums= [12,33,56,78,9]
+
+print(nums)
+
+a= nums.sort()
+print(a)
+
+tup = (12,11,13,7)
+print(tup)
+
+#dict = {1:'Asif',2:'Quasmi',3:'JS'}
+#print(dict)
+#print(dict[2])
+
+#print(dict.get(4,'Not found'))
+
+key =[1,2,3,4]
+values=[1,4,9,16]
+
+ 
+
+dictno = dict(zip(key,values))
+print(dictno)
+
+dictno[10]=56
+print(dictno)
+
+abc
