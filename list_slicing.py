c9c2d058e7ed425fc3a346ac98e06295b4dbfb98
Asif Quasmi <asifquasmi11@gmail.com>
2024-03-09 14:58:57 +0530

    string slicing

--- list_slicing.py
+++ list_slicing.py
@@ -1,13 +1,15 @@
 from loguru import logger
 
-string1 = " Ralph Kimball founded the Kimball Group. Since the mid-1980s, he has been the data warehouse and business intelligence industry’s thought leader on the dimen"
+string1 = " Ralph Kimball founded the Kimball Group. Since the mid-1980s, the THE been The data warehouse and business intelligence industry’s thought leader on the dimen"
 logger.info(string1)
 
 list_slicing = string1.split(" ")
+list_slicing_lower = string1.lower().split(" ")
+logger.info(list_slicing_lower)
 # count = list_slicing.count("the")
 
 count = 0
-for i in list_slicing:
+for i in list_slicing_lower:
     if i == 'the':
         count = count + 1
-logger.info(count)
+logger.info(f"Total count of the is = {count} ")
