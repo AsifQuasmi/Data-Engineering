415cef0d5da040c0afbd50968db7664271a457fc
Asif Quasmi <asifquasmi11@gmail.com>
2024-03-22 01:21:46 +0530

    Singly Linked list

---
+++ Data Strcture and Algoritham/Singly_linked_list.py
@@ -0,0 +1,50 @@
+"""Singly linked list"""
+
+
+class Node:
+    def __init__(self,items=None,next=None):
+        self.items = items
+        self.next = next
+
+
+class sll:
+    def __init__(self,start=None):
+        self.start = start
+    def is_empty(self):
+        return self.start == None
+    def insert_at_start(self,data):
+        n = Node(data,self.start)
+        self.start = n
+    def insert_at_last(self,data):
+        n = Node(data)
+        if not self.is_empty():
+            temp = self.start
+            while(temp.next is not None):
+                temp = temp.next
+            temp.next = n
+        else:
+            self.start = n
+    def search(self,data):
+        temp = self.start
+        while temp is not None:
+            if temp.items==data:
+                return temp
+            temp=temp.next
+        return None
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n = Node(data,temp.next)
+            temp.next = n
+    def print_list(self):
+        temp=self.start
+        while temp is not None:
+            print(temp.items, end= " ")
+            temp = temp.next
+
+
+# Driver code
+        
+list1 = sll()
+list1.insert_at_last(30)
+list1.insert_at_start(12)
+list1.print_list()
